dfcda3e5b0c275f34a4e73ee4e3b1a701fe74e86
adewobiadetayo1@gmail.com <adewobiadetayo1@gmail.com>
2023-09-12 07:11:00 +0100

    model

--- model/person.js
+++ model/person.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 personSchema = new Schema({
-  name: { type: String, unique: true, trim: true },
+  name: { type: String, unique: true, lowercase: true, trim: true },
 });
 
 const Person = mongoose.model("Person", personSchema);
